1419bf7973b662331ef1ca365c1067f81e7dc4de
Pengfei Chen <pfc888@gmail.com>
2021-02-10 05:15:46 +0900

    Conform Theme to Codable, changed color from Color to a struct, created API to convert RBG struct to Color.

--- Memorize/View/Theme.swift
+++ Memorize/View/Theme.swift
@@ -6,22 +6,22 @@
 //
 import SwiftUI
 
-struct Theme {
+struct Theme: Codable {
     var name: String
     var emoji: [String]
-    var color: Color
-    var numberOfCards: Int?
+    var color: UIColor.RGB
+    var numberOfCards: Int
+    
+    func foregroundColor() -> Color {
+        Color(self.color)
+    }
 }
 
 let themes: [Theme] = [
-Theme(name: "Animals", emoji: ["🐱","🐶","🐼","🐮","🐸","🦊"], color: .green, numberOfCards: 4),
-
-Theme(name: "Sports", emoji: ["⚽️","🏀","🏈","⚾️","🎾","🏐"], color: .blue, numberOfCards: 4),
-
-Theme(name: "Faces", emoji: ["😯","😬","🤭","🤗","🤤","🙄"], color: .yellow, numberOfCards: 4),
-
-Theme(name: "Flags", emoji: ["🇨🇳","🇺🇸","🇸🇬","🇯🇵","🇰🇷","🇦🇺"], color: .gray),
-
-Theme(name: "Food", emoji: ["🥦","🌽","🌶","🧄","🧀","🍔"], color: .red, numberOfCards: 4),
-
-Theme(name: "Halloween", emoji: ["👻","🎃", "🕸","🍫", "❤️"], color: .orange)]
+    Theme(name: "Animals", emoji: ["🐱","🐶","🐼","🐮","🐸","🦊"], color: UIColor(.green).rgb, numberOfCards: 4),
+    Theme(name: "Sports", emoji: ["⚽️","🏀","🏈","⚾️","🎾","🏐"], color: UIColor(.blue).rgb, numberOfCards: 4),
+    Theme(name: "Faces", emoji: ["😯","😬","🤭","🤗","🤤","🙄"], color: UIColor(.yellow).rgb, numberOfCards: 4),
+    Theme(name: "Flags", emoji: ["🇨🇳","🇺🇸","🇸🇬","🇯🇵","🇰🇷","🇦🇺"], color: UIColor(.gray).rgb,numberOfCards: 6),
+    Theme(name: "Food", emoji: ["🥦","🌽","🌶","🧄","🧀","🍔"], color: UIColor(.red).rgb, numberOfCards: 4),
+    Theme(name: "Halloween", emoji: ["👻","🎃", "🕸","🍫", "❤️"], color: UIColor(.orange).rgb, numberOfCards: 5)
+]
